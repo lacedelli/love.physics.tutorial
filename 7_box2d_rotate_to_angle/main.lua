84cd35ba59735cf54f25463125537710cfd5cd8f
lacedelli <lacedelli@outlook.com>
2022-05-04 12:55:46 -0500

    Add mouse click function

--- 7_box2d_rotate_to_angle/main.lua
+++ 7_box2d_rotate_to_angle/main.lua
@@ -14,12 +14,22 @@ function love.load()
   points[8] = points[8] * 4
   local shape = phys.newPolygonShape(points)
 
+  -- Just the usual body initiation stuff
   obj = {body = phys.newBody(world, love.graphics.getWidth() / 2, 
     love.graphics.getHeight() / 2, "dynamic")}
   obj.fixture = phys.newFixture(obj.body, shape)
+
+  -- Settings for our pointer
+  mouse = {0,0}
+  love.graphics.setPointSize(8)
+end
+
+function love.mousepressed(x, y, b)
+  if b == 1 then mouse[1], mouse[2] = x, y end
 end
 
 function love.update(dt)
+  world:update(dt)
 end
 
 function love.draw()
@@ -27,4 +37,7 @@ function love.draw()
   love.graphics.polygon("fill", obj.body:getWorldPoints(obj.fixture:getShape():getPoints()))
   love.graphics.setColor(.9, .5, .5, .9)
   love.graphics.polygon("line", obj.body:getWorldPoints(obj.fixture:getShape():getPoints()))
+
+  love.graphics.setColor(1,1,1)
+  love.graphics.points(mouse)
 end
